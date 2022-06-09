04977201c01c8d01912fc37dce2fa02ab601e888
James Hyatt <jameshyattdevelpment@Jamess-iMac.lan>
2022-06-09 12:00:31 -0400

    first commit

---
+++ pages/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className='headerContainer'>
+        <a href="/"><h2>James Hyatt</h2></a>
+        <div className="nav">
+            <a href="/about-james">Meet James</a>
+            <a href="/my-brain">My Brain</a>           
+        </div>
+    </div>
+  )
+}
+
+export default index
